0c367f5ec96fe08cf56a5cf16b7fc6641e37d5be
BitWizCoder <mhmdnoman01@gmail.com>
2026-01-25 01:45:51 +0600

    added middleware

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -47,6 +47,7 @@ export const auth = betterAuth({
 
   emailVerification: {
     sendOnSignUp: true,
+    autoSignInAfterVerification: true,
     sendVerificationEmail: async ({ user, url, token }, request) => {
       try {
         const verificationUrl = `${process.env.APP_URL}/verify-email?token=${token}`;
@@ -89,6 +90,15 @@ export const auth = betterAuth({
         console.error(error);
         throw error;
       }
+    },
+  },
+
+  socialProviders: {
+    google: {
+      prompt: "select_account consent",
+      accessType: "offline",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
 });
